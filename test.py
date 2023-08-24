dd6592ac897638297a37f41bbb6e7c094b9479aa
Maxime Beretvas <100215730+maxime-beretvas@users.noreply.github.com>
2023-08-24 19:52:27 +0200

    .

--- test.py
+++ test.py
@@ -3,22 +3,16 @@ import serial
 # Open the serial port
 ser = serial.Serial('/dev/ttyUSB0', 115200)
 
-# Open a log file
-log_file = open('serial_log.txt', 'a')
-
 try:
     while True:
         # Read data from the serial port
         data = ser.readline()
-        
-        # Write the data to the log file
-        log_file.write(data.decode('utf-8'))  # Assuming data is in UTF-8 encoding
-        
+
         # Optionally, print the data to the console
         print(data.decode('utf-8'))
+
 except KeyboardInterrupt:
     print("Capture stopped by user")
 finally:
     # Close the serial port and log file
     ser.close()
-    log_file.close()
