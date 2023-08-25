f77333b55dd57f1f908c50a7630c700fc9ddba6c
Maxime Beretvas <100215730+maxime-beretvas@users.noreply.github.com>
2023-08-25 19:17:58 +0200

    .

--- test2.py
+++ test2.py
@@ -56,9 +56,10 @@ def main():
                 line = line.strip(")\r")
 
                 x = line.split("(")
+                print(x)
                 try:
                     for item in x[1:]:
-                        print(item)
+
                         stripped_item = item.strip(")")
                         # print("\n",len(stripped_item)," = ",stripped_item,"\n")
                         
