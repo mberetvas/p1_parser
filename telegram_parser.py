370a65b7d7b089fb8cc80e9ce6069181140deb20
Maxime Beretvas <100215730+maxime-beretvas@users.noreply.github.com>
2023-08-27 18:53:58 +0200

    .

--- telegram_parser.py
+++ telegram_parser.py
@@ -1,39 +1,10 @@
 import serial
-import binascii
 
 def crc16(data):
-    """Calculates the CRC-16 of the given data."""
-    crc = 0xFFFF
-    for byte in data:
-        crc ^= byte
-        for i in range(8):
-            if crc & 1:
-                crc >>= 1
-                crc ^= 0xEDB88320
-            else:
-                crc >>= 1
-    return crc
+    pass
 
 def checkcrc(telegram):
-    """Checks the CRC code of the given telegram."""
-    try:
-        if len(telegram) < 2:
-            return False
-
-        # Find the position of '!' in the telegram
-        exclamation_index = telegram.find(b'!')
-
-        if exclamation_index != -1:
-            expected_crc = binascii.unhexlify(telegram[exclamation_index + 1:exclamation_index + 5])
-            print("expected crc = ",expected_crc)
-            calculated_crc = crc16(telegram[:exclamation_index + 1])
-            print("calculated_crc = ",calculated_crc)
-
-            if calculated_crc == expected_crc:
-                return True
-        return False
-    except:
-        print("error in checkcrc(telegram)")
+    pass
 
 
 def parse_telegram(telegram):
@@ -66,10 +37,13 @@ def main():
             telegram.extend(p1_line)
             
             if p1_line.decode('ascii').startswith('!'):
+                data = parse_telegram(telegram)
+                print(data)
                 # telegram complete (end of telegram is the crc code exampl: "!hex code")
                 if checkcrc(telegram):
                     data = parse_telegram(telegram)
                     print(data)
+
                 else:
                     print("CRC error")
 
