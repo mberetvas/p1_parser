389351452f390fbf2c5433ff80620111c8c61007
Maxime Beretvas <100215730+maxime-beretvas@users.noreply.github.com>
2023-08-27 19:18:53 +0200

    .

--- telegram_parser.py
+++ telegram_parser.py
@@ -32,6 +32,7 @@ def main():
             if "/" in decoded_p1:
                 telegram = bytearray()
             telegram.extend(p1_line)
+            print("\n")
             print(telegram)
 
 if __name__ == "__main__":
