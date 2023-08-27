7e59a22b538313063721d91b9595809a1578bb1f
Maxime Beretvas <100215730+maxime-beretvas@users.noreply.github.com>
2023-08-27 19:27:19 +0200

    .

--- telegram_parser.py
+++ telegram_parser.py
@@ -1,7 +1,21 @@
 import serial
 
+
 def crc16(data):
-    pass
+    crc = 0xFFFF
+    polynomial = 0x1021
+
+    for byte in data:
+        for bit in range(8):
+            if (crc & 0x01):
+                crc = (crc >> 1) ^ polynomial
+            else:
+                crc >>= 1
+
+            byte >>= 1
+
+    return crc
+
 
 def checkcrc(telegram):
     pass
@@ -9,6 +23,7 @@ def checkcrc(telegram):
 
 def parse_telegram(telegram):
     pass
+
 
 def main():
     # Create a configuration dictionary
@@ -19,10 +34,10 @@ def main():
         'parity': serial.PARITY_NONE,
         'stopbits': serial.STOPBITS_ONE
     }
-    
+
     with serial.Serial(**config) as ser:
         while True:
-            
+
             # read data
             p1_line = ser.readline()
             decoded_p1 = p1_line.decode('ascii')
@@ -34,10 +49,17 @@ def main():
                 telegram.extend(p1_line)
             except:
                 continue
-            
+
             if "!" in decoded_p1:
                 # print complete telegram
+                print("\n")
                 print(telegram)
+                
+                try:
+                    print(crc16(telegram))
+                except:
+                    print("crc nok")
+
 
 if __name__ == "__main__":
     main()
