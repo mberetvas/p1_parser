1bf2f4efa57c6776cba8dc955dc9e67b426908c0
Maxime Beretvas <100215730+maxime-beretvas@users.noreply.github.com>
2023-08-31 09:43:52 +0200

    .

--- test4.py
+++ test4.py
@@ -12,7 +12,7 @@ def calculate_crc16(telegram):
         crc ^= byte << 8
         for _ in range(8):
             if crc & 0x8000:
-                crc = (crc << 1) ^ IBM_POLYNOMIAL
+                crc = (crc << 1) ^ IBM_POLY_reversed
             else:
                 crc <<= 1
             crc &= 0xFFFF  # Ensure the result stays 16 bits
