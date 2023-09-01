df696045ed2137fce5db8cda8c1945441605d95a
Maxime Beretvas <100215730+maxime-beretvas@users.noreply.github.com>
2023-09-01 23:00:47 +0200

    .

--- test5.py
+++ test5.py
@@ -3,12 +3,13 @@ import crcmod
 import time
 import re
 import pandas
+from datetime import datetime, timezone
 
 
 # Dict with obiscode description (24) to add or remove from database comment line out
 obiscodes = {
-    "0-0:96.1.4":"verson_info",  # Versie informatie xxxyy waar xxx=dsmr p1 versie en yy=e-MUCs-h versie
-    "0-0:96.1.1":"equipment_id", # Hex encoded equip. id according to DIN 43863-5
+    #"0-0:96.1.4":"verson_info",  # Versie informatie xxxyy waar xxx=dsmr p1 versie en yy=e-MUCs-h versie
+    #"0-0:96.1.1":"equipment_id", # Hex encoded equip. id according to DIN 43863-5
     "0-0:1.0.0":"timestamp", # Timestamp YYMMDDhhmmssX X is S=Summer W=winter
     "1-0:1.8.1":"totaal_verbruik_dagtarief_kwh",
     "1-0:1.8.2":"totaal_verbruik_nachttarief_kwh",
@@ -16,7 +17,7 @@ obiscodes = {
     "1-0:2.8.2":"Totale_injectie_nachttarief_kwh",
     "0-0:96.14.0":"Tarief_indicatie", # tarief_indicatie
     "1-0:1.4.0":"gemmiddeld_verbruik_kw", # Gemmiddeld verbruik kw
-    #"1-0:1.6.0":"piekvermogen_huidige_maand", # Maximum piekvermogen voor huidige maand
+    "1-0:1.6.0":"piekvermogen_huidige_maand", # Maximum piekvermogen voor huidige maand
     #"0-0:98.1.0":"piekvermogen _van_laatste_13_maanden",
     "1-0:1.7.0":"actief_verbruik_kw", # actief verbruik in kw
     "1-0:2.7.0":"actief_injectie_kw", # actieve injectie in kw
@@ -24,13 +25,13 @@ obiscodes = {
     "1-0:22.7.0":"instant_vermogen_L1-P_kw", # instantaneous active power L1 -P
     "1-0:32.7.0":"spanning_V", # instantaneous voltage
     "1-0:31.7.0":"stroom_A", # instantaneous current
-    "0-0:96.3.10":"stand_zekering", # Breaker state 0=Disconnected 1=connected 3=ready for connection
-    "0-0:17.0.0":"Limiter_threshold_kw", # limiter_threshold
-    "1-0:31.4.0":"fuse_threshold_A", # fuse_treshold
-    "0-0:96.13.0":"Text_Messag", # (for future use)
+    #"0-0:96.3.10":"stand_zekering", # Breaker state 0=Disconnected 1=connected 3=ready for connection
+    #"0-0:17.0.0":"Limiter_threshold_kw", # limiter_threshold
+    #"1-0:31.4.0":"fuse_threshold_A", # fuse_treshold
+    #"0-0:96.13.0":"Text_Messag", # (for future use)
     #"0-1:24.1.0":"Device_type", # device-type
     #"0-1:24.4.0":"gasklep_stand" # gasklep_stand
-    #"0-1:24.2.3":"gas_verbruik_m³" # Last value of 'not temperature corrected' gas volume in m³,including decimal values and capture time
+    "0-1:24.2.3":"gas_verbruik_m³" # Last value of 'not temperature corrected' gas volume in m³,including decimal values and capture time
 }
 
 # serial port config dictionary
@@ -57,6 +58,11 @@ def crc16(data):
     crc = (~crc & 0xFFFF)
     crc = (crc << 8) | ((crc >> 8) & 0xFF)
     return crc & 0xFFFF
+
+def convert_to_utc(timestamp_str):
+    timestamp = datetime.strptime(timestamp_str, '%Y%m%d%H%M%S%S')
+    utc_timestamp = timestamp.replace(tzinfo=timezone.utc).timestamp()
+    return utc_timestamp
 
 
 def read_telegram():
