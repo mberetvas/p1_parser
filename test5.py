ed4f5ad832ac04e52dbddcc36c6c55b368b7f5bc
Maxime Beretvas <100215730+maxime-beretvas@users.noreply.github.com>
2023-09-01 18:45:18 +0200

    .

--- test5.py
+++ test5.py
@@ -5,6 +5,34 @@ import re
 import pandas
 
 
+# Dict with obiscode description (24)
+obiscodes = {
+    "0-0:96.1.4":"verson_info",  # Versie informatie xxxyy waar xxx=dsmr p1 versie en yy=e-MUCs-h versie
+    "0-0:96.1.1":"equipment_id", # Hex encoded equip. id according to DIN 43863-5
+    "0-0:1.0.0":"timestamp", # Timestamp YYMMDDhhmmssX X is S=Summer W=winter
+    "1-0:1.8.1":"totaal_verbruik_dagtarief_kwh",
+    "1-0:1.8.2":"totaal_verbruik_nachttarief_kwh",
+    "1-0:2.8.1":"totaal_injectie_dagtarief_kwh",
+    "1-0:2.8.2":"Totale_injectie_nachttarief_kwh",
+    "0-0:96.14.0":"Tarief_indicatie", # tarief_indicatie
+    "1-0:1.4.0":"gemmiddeld_verbruik_kw", # Gemmiddeld verbruik kw
+    "1-0:1.6.0":"piekvermogen_huidige_maand", # Maximum piekvermogen voor huidige maand
+    "0-0:98.1.0":"piekvermogen _van_laatste_13_maanden",
+    "1-0:1.7.0":"actief_verbruik_kw", # actief verbruik in kw
+    "1-0:2.7.0":"actief_injectie_kw", # actieve injectie in kw
+    "1-0:21.7.0":"instant_vermogen_L1+P_kw", # instantaneous active power L1 +P
+    "1-0:22.7.0":"instant_vermogen_L1-P_kw", # instantaneous active power L1 -P
+    "1-0:32.7.0":"spanning_V", # instantaneous voltage
+    "1-0:31.7.0":"stroom_A", # instantaneous current
+    "0-0:96.3.10":"stand_zekering", # Breaker state 0=Disconnected 1=connected 3=ready for connection
+    "0-0:17.0.0":"Limiter_threshold_kw", # limiter_threshold
+    "1-0:31.4.0":"fuse_threshold_A", # fuse_treshold
+    "0-0:96.13.0":"Text_Messag", # (for future use)
+    "0-1:24.1.0":"Device_type", # device-type
+    "0-1:24.4.0":"gasklep_stand", # gasklep_stand
+    "0-1:24.2.3":"gas_verbruik_m³" # Last value of 'not temperature corrected' gas volume in m³,including decimal values and capture time
+}
+
 # serial port config dictionary
 SERIAL_CONFIG = {
     'port': '/dev/ttyUSB0',
@@ -69,7 +97,8 @@ def parse_telegram(message):
                 idx = line.index("(")
                 obis_code = line[:idx]
                 value = line[idx:].replace("(", " (").split()
-                parsed_telegram[obis_code] = value
+                if obis_code in obiscodes:
+                    parsed_telegram[obiscodes[obis_code]] = value
             except:
                 continue
     return parsed_telegram
