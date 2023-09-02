9cd85a6c939ea6f8294239b47020ac3ffdfbd230
Maxime Beretvas <100215730+maxime-beretvas@users.noreply.github.com>
2023-09-02 12:18:56 +0200

    .

--- test5.py
+++ test5.py
@@ -250,7 +250,7 @@ def parse_telegram(message):
                         else:
                             parsed_telegram[obiscodes[obis_code]] = value[0].strip('()')
             except Exception as e:
-                print(f"{e} = line")
+                print(f"{e} / line = {line}")
                 continue
     return parsed_telegram
 
